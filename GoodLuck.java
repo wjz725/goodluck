a0ce51abc4cf2b0617356596bb7e251e04da6513
wjz725 <48241513+wjz725@users.noreply.github.com>
2019-03-05 17:50:22 +0800

    Add files via upload

---
+++ GoodLuck.java
@@ -0,0 +1,11 @@
+
+public class GoodLuck {
+
+	public static void main(String[] args) {
+		System.out.println("Gook Luck");
+		System.out.println("Gook Luck2");
+		System.out.println("Gook Luck3");
+		System.out.println("Gook Luck4");
+	}
+	
+}
